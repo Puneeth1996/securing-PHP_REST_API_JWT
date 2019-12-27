b0413632ce13168f4e498dad4ef4e9fb47556409
Puneeth1996 <puneeth1996p@gmail.com>
2019-12-27 17:19:54 +0530

    Should Try Integrating the JWT Part

--- objects/product.php
+++ objects/product.php
@@ -1,20 +1,13 @@
 <?php
 class Product{
- 
+
     // database connection and table name
     private $conn;
-    private $table_name = "view_backend";
+    private $table_name = "actor";
     // object properties
-    public $id;
-    public $Blog_unique_id;
-    public $Publish_date;
-    public $Main_title;
-    public $Sub_title;
-    public $Author;
-    public $category;
-    public $desp_small;
-    public $desp_full;
-    public $img;
+    public $fullname;
+    public $actor_id;
+    public $last_update;
     // constructor with $db as database connection
     public function __construct($db){
         $this->conn = $db;
@@ -23,17 +16,17 @@ class Product{
 function read(){
     
     $query = "SELECT 
-                    id,Blog_unique_id,Publish_date,Main_title,Sub_title,Author,category,desp_small,desp_full,img
+                    fullname,actor_id,last_update
                 FROM 
                     ".$this->table_name."
             ";
- 
+
     // prepare query statement
     $stmt = $this->conn->prepare($query);
- 
+
     // execute query
     $stmt->execute();
- 
+
     return $stmt;
 }
 }
